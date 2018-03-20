56c005ff9bfd81ed75592a9666626062bf09d8f5
Kristofer Heyward <krisroc7@gmail.com>
2018-03-20 22:22:36 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
-  
+  $('button').click(function(){
+
+    var input = $('input').val();
+    
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=" +  input + "&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+        console.log(response);
+        console.log(response.data[0].images.original.url)
+        $("body").append("<img src=" + response.data[2].images.original.url + "/>")
+    
+        
+      },
+    }); 
+
+
+});
   
   
   
